e710b05e6e9bf8d1b3374e34d5bb471b3e601b83
unknown <shahketan1810@gmail.com>
2022-02-21 00:25:34 +0530

    added rxjs for smooth scroll and header

--- client/src/PortfolioContainer/Home/Home.js
+++ client/src/PortfolioContainer/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Header from './Header/Header';
 import Profile from './Profile/Profile';
 import Footer from './Footer/Footer';
 
@@ -8,6 +9,7 @@ import './Home.css';
 export default function Home() {
   return (
     <div className="home-container">
+        <Header/>
         <Profile/>
         <Footer/>
     </div>
