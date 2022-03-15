df05d06a8c8458926ead7bf079a62c85dc043849
unknown <shahketan1810@gmail.com>
2022-03-15 23:44:03 +0530

    styles contact me

--- client/src/PortfolioContainer/ContactMe/ContactMe.js
+++ client/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Typical from 'react-typical';
 
 import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
 import ScrollService from '../../utilities/ScrollService';
 import Animations from '../../utilities/Animations';
 
 import imgBack from '../../assets/ContactMe/mailz.jpeg';
+
+import './ContactMe.css';
 
 export default function (props) {
 
@@ -16,6 +19,24 @@ export default function (props) {
 
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [banner, setBanner] = useState("");
+    const [bool, setBool] = useState(false);
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    }
+    
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+    
+    const handleMessage = (e) => {
+        setMessage(e.target.value);
+    }
+
     return (
         <div className="contact-me-container" id={props.id || ""}>
             <ScreenHeading 
@@ -24,7 +45,52 @@ export default function (props) {
             />
             <div className="central-form">
                 <div className="col">
-                    
+                    <h2>
+                        <Typical 
+                            loop={Infinity}
+                            steps={[
+                                "Get In Touch 🤟",
+                                1000,
+                                "Email Me 📧",
+                                1000
+                            ]}
+                        />
+                    </h2>
+                    <div className="colz-icon">
+                        <a href='#'>
+                            <i className='fa fa-linkedin-square'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-github-square'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-envelope-square'></i>
+                        </a>
+                    </div>
+                </div>
+                <div className="back-form">
+                    <div className="img-back">
+                        <h4>Send Your Email Here!</h4>
+                        <img src={imgBack} alt="Not found" />
+                    </div>
+                    <form action="">
+                        <p>{banner}</p>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" onChange={handleName} value={name}/>
+                        
+                        <label htmlFor="email">Email</label>
+                        <input type="email" onChange={handleEmail} value={email}/>
+                        
+                        <label htmlFor="message">Message</label>
+                        <textarea type="text" onChange={handleMessage} value={message}/>
+
+
+                        <div className="send-btn">
+                            <button type='submit'>
+                                send <i className="fa fa-paper-plane" />
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- client/src/PortfolioContainer/Projects/Projects.js
+++ client/src/PortfolioContainer/Projects/Projects.js
@@ -27,6 +27,11 @@ export default function Projects(props) {
                 <ScreenHeading title={'Projects'} subHeading={"Here's my Recent Work"} />
                 <ProjectView />
             </div>
+            <div class="custom-shape-divider-bottom-1647360679">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M892.25 114.72L0 0 0 120 1200 120 1200 0 892.25 114.72z" class="shape-fill"></path>
+                </svg>
+            </div>
         </div>
     );
 }
